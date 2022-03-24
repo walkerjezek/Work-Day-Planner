973b17c91a966ab0721f8dc0213259efecdad56e
walkerj <wpjezek@gmail.com>
2022-03-24 16:58:20 -0600

    added basic planner layout styling

---
+++ script.js
@@ -0,0 +1,25 @@
+// Global Variables
+// Times
+var am9 = 9;
+var am10 = 10;
+var am11 = 11;
+var pm12 = 12;
+var pm1 = 13;
+var pm2 = 14;
+var pm3 = 15;
+var pm4 = 16;
+var pm5 = 17;
+
+//-----------------------------------------------------------------
+//Todays date
+var today = moment();
+$("#currentDay").text(today.format("LL"));
+
+
+//-----------------------------------------------------------------
+// Function to add background based on time of day
+var currentTime = moment();
+
+function {
+    if (currentTime == 
+}
