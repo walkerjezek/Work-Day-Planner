3884c9514fed283a17dcc55ad5e5fdbe42e7bcf4
walkerj <wpjezek@gmail.com>
2022-03-25 21:19:31 -0600

    Making final tweaks. Room to optimize

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // Global Variables
-// Times
-// var am9 = 9;
-// var am10 = 10;
-// var am11 = 11;
-// var pm12 = 12;
-// var pm1 = 13;
-// var pm2 = 14;
-// var pm3 = 15;
-// var pm4 = 16;
-// var pm5 = 17;
 // var hourBlock = $('.description');
 
 // Gather the elements
